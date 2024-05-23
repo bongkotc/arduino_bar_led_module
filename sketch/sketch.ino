8b3f3aede4ca5b1a3522dbb6a1de379a57c30740
bongkotc <bongkot.chai@gmail.com>
2024-05-23 22:20:24 +0700

    Update Code

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,9 +1,36 @@
 #include <Arduino.h>
-void setup() {
-  Serial.begin(9600);
+
+// LED Active Low
+#define MAX_LED 8
+
+const int ledPinNo[MAX_LED] = {2, 3, 4, 5, 6, 7, 8, 9}; // LED Output Pins
+void setup()
+{
+    for (int i = 0; i < MAX_LED; i++)
+    {
+        pinMode(ledPinNo[i], OUTPUT);    // Init Output
+        digitalWrite(ledPinNo[i], HIGH); // Init Value
+    }
 }
 
-void loop() {
-  Serial.println("Hello");
-  delay(1000);
+void ledOnAll(){
+    for (int i = 0; i < MAX_LED; i++)
+    {
+        digitalWrite(ledPinNo[i], LOW); //ON
+    }
+}
+
+void ledOffAll(){
+    for (int i = 0; i < MAX_LED; i++)
+    {
+        digitalWrite(ledPinNo[i], HIGH); //ON
+    }
+}
+
+void loop()
+{
+    ledOnAll();
+    delay(1000);
+    ledOffAll();
+    delay(1000);
 }
